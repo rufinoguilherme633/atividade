b3238799366af905c1592fb5cc8dff2927aa8fd9
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-03-30 11:21:59 -0300

    t

--- functios.js
+++ functios.js
@@ -1,19 +1,25 @@
-    // 'use strict'
-    // import {alunos} from "./alunos.js"
+    'use strict'
+    import {alunos} from "./alunos.js"
 
 
     // const criarAlunos = (aluno) => {
     //     const alunos = document.createElement('div')
-    //     alunos.classList.add('alunos')
+    //     alunos.classList.add('containerAlunos')
 
-    //     //  const img = document.createElement('img')
-    //     //  img.classList.add('imgAluno')
-    
+    //     const containerAluno = document.createElement('div');
+    //     containerAluno.classList.add('containerAluno');
+       
+    //       const img = document.createElement('img')
+    //      img.classList.add('imgAluno')
+    //      img.src = `./img/aluno.png`
 
     //     const name = document.createElement('span')
     //     name.classList.add('nameAluno')
     //    name.textContent = aluno.nome
-    // alunos.append(name)
+    // alunos.append(name, img)
+    // alunos.append(containerAluno)
+  
+
     // return alunos
     // }
 
@@ -21,10 +27,79 @@
     //     const container = document.getElementById('alunos')
     //     const cntatoI = alunos.map(criarAlunos)
 
+        
     //     container.replaceChildren(...cntatoI)
     // }
 
 
     //  carregarAlunos()
+    let idIncremental = 1; // variável para gerar um ID único para cada div de aluno
+     const criarAlunos = (aluno) => {
+        const containerAlunos = document.createElement('div');
+        containerAlunos.classList.add('containerAlunos');
+        
+        const containerAluno = document.createElement('div');
+        containerAluno.classList.add('containerAluno');
 
-    // // -----------------------------------------------
+        // containerAluno.id = 'containerAluno'
+
+        containerAluno.id = `containerAluno-${idIncremental}`; // utiliza a variável incremental para gerar um ID único
+
+        if(aluno.status  === 'Finalizado') {containerAluno.style.backgroundColor = '#3347B0'}
+        else{  containerAluno.style.backgroundColor = ' #E5B657'}
+
+       
+
+         
+    
+        const img = document.createElement('img')
+        img.classList.add('imgAluno')
+        img.src = `./img/aluno.png`
+    
+        const name = document.createElement('span')
+        name.classList.add('nameAluno')
+        name.textContent = aluno.nome
+    
+        containerAluno.append(img, name);
+        containerAlunos.append(containerAluno);
+        
+ idIncremental++; // incrementa a variável incremental para gerar um novo ID único para a próxima div de aluno
+
+        return containerAlunos;
+    }
+    
+    const carregarAlunos = () => {
+        const container = document.getElementById('alunos');
+        const cntatoI = alunos.map(criarAlunos);
+        container.append(...cntatoI);
+    }
+    
+    carregarAlunos();
+
+    // -----------------------------------------------
+
+
+    const aluno = () => {
+        const containerAlunos = document.createElement('div');
+        containerAlunos.classList.add('alunoselecionado');
+       
+
+        const img = document.createElement('img')
+        img.classList.add('imgalunoescolhido')
+        img.src = `./img/aluno.png`
+
+        const name = document.createElement('span')
+        name.classList.add('namealunoselecionado')
+        name.textContent = aluno.nome
+
+        containerAluno.append(img, name);
+        
+    }
+
+    const carregaAlunoIndividual = () => {
+        const container = document.getElementById('alunoselecionado');
+        const cntatoI = alunos.map(criarAlunos);
+        container.append(...cntatoI);
+    }
+
+    carregaAlunoIndividual()
--- teste.js
+++ teste.js
@@ -7,10 +7,20 @@
 				divPrincipal.addEventListener("click", function () {
 					if (divOpcoes.style.display === "none") {
 						divOpcoes.style.display = "block";
+						
 					} else {
 						divOpcoes.style.display = "none";
 					}
 				});
+
+				divOpcoes.addEventListener("click", function () {
+					if (divOpcoes.textContent = "Status") {
+						main2.backgroundColor = 'red'
+					} else {
+						
+					}
+				});
+		
 		
 
 // ---------------------------------------------------------------------------
@@ -18,52 +28,102 @@
 			const room = document.querySelectorAll('.room');
 			const main = document.getElementById('principal');
 			const main2 = document.getElementById('segundario');
+
+			main2.style.overflow = 'auto'
+			main2.style.height ='100%'
+
+		
+		const main3 = document.getElementById('terciario')
 			const span = document.getElementById("t");
+			const headerSecondary = document.getElementById('headerSecondary')
 
-
+			
+			// main3.style.overflow = 'auto'
+			// main3.style.height ='100%'
 			room.forEach(function (element) {
 				console.log(element)
 				element.addEventListener('click', function () {
 
 					main.style.display = 'none';
+					headerSecondary.style.display = "flex"
 					main2.style.display = 'block';
 
 					const span =document.getElementById("t");
 					if(span.textContent ===  "Sair") {
 						span.textContent = "voltar"
 					}else {
-						span.textContent = "sair"
+						span.textContent = "Sair"
 					}
+// Obter o texto da classe a ser usado como novo texto
+const newClassName = this.querySelector(".nameClassRoom").textContent;
+
+// Atualizar o texto da classe nameClassRoom com o novo texto
+const nameClassRoom = document.querySelector(".nameClassRoomescolhida");
+nameClassRoom.textContent =newClassName;
+					
 				});
 			});
 			span.addEventListener('click', function () {
-				if(span.textContent ==="voltar") {
+				if(span.textContent ==="Sair") {
+
+
+				}else{
+					if(span.textContent ==="voltar" && main2.style.display == 'block') {
 					
-				main.style.display = 'block';
-				main2.style.display = 'none';
-				}else {
+						main.style.display = 'flex';
+						headerSecondary.style.display = "none"
+						main3.style.display = 'none'
+						main2.style.display = 'none';
+						span.textContent = "Sair"
+		
+		
+						
+		
+						}
+						
+						else {
+							main2.style.display = 'block';
+							main3.style.display = 'none'
+							main.style.display = 'none';
+						}
 
 				}
+
+			
+
 
 				
 			  });
 		
-
 //----------------------------------------------------
 
 
-			const containerAlunos = document.querySelectorAll('.containerAlunos');
+			const containerAluno = document.querySelectorAll('.containerAlunos');
 			const segundario = document.getElementById('segundario');
 			const terciario = document.getElementById('terciario');
+			// const headerSecondary = document.getElementById('headerSecondary')
 
-			containerAlunos.forEach(function (element) {
+
+			// containerAluno.forEach(function (element) {
+			// 	element.addEventListener('click', function (event) {
+			// 	  var alunoId = event.target.id; // obtém o id do aluno clicado
+			// 	  var alunoInfo = document.getElementById(alunoId + '-info'); // obtém as informações específicas do aluno
+			// //    alunoInfo.style.display = 'block'; // exibe as informações
+			// 	console.log(alunoInfo)
+			// 	});
+			//   });
+
+
+
+			containerAluno.forEach(function (element) {
 				console.log(element)
 				element.addEventListener('click', function () {
 					segundario.style.display = 'none';
-					terciario.style.display = 'block';
+					terciario.style.display = 'flex';
+					headerSecondary.style.display = 'none';
 				});
 			});
-//----------------------------------------------------------------------
+// //----------------------------------------------------------------------
 
 
 
