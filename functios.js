e6f2de7eb803cc284a173e6ed7b61d5324834504
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-04-05 11:05:36 -0300

    implementacao da api na primeira tela

--- functios.js
+++ functios.js
@@ -8,7 +8,7 @@
 
 //     //     const containerAluno = document.createElement('div');
 //     //     containerAluno.classList.add('containerAluno');
-       
+
 //     //       const img = document.createElement('img')
 //     //      img.classList.add('imgAluno')
 //     //      img.src = `./img/aluno.png`
@@ -18,7 +18,7 @@
 //     //    name.textContent = aluno.nome
 //     // alunos.append(name, img)
 //     // alunos.append(containerAluno)
-  
+
 
 //     // return alunos
 //     // }
@@ -27,7 +27,7 @@
 //     //     const container = document.getElementById('alunos')
 //     //     const cntatoI = alunos.map(criarAlunos)
 
-        
+
 //     //     container.replaceChildren(...cntatoI)
 //     // }
 
@@ -36,34 +36,34 @@
 //     const criarMateria = (materia) => {
 //         const room = document.createElement("div");
 //         room.classList.add("room");
-      
+
 //         const img = document.createElement('img');
 //         img.classList.add('imgAula');
 //         img.src = materia.icone;
-      
+
 //         const name = document.createElement('span');
 //         name.classList.add('nameClassRoom');
 //         name.textContent = materia.nome;
-      
+
 //         room.append(img, name);
-      
+
 //         return room;
 //       }
-      
+
 //       const carregarAula = () => {
 //         const container = document.getElementById('classRoom');
 //         const cntatoI = alunos[0].curso.map(criarMateria);
 //         container.append(...cntatoI);
 //       }
-      
+
 //       carregarAula();
-      
+
 
 //     let idIncremental = 1; // variável para gerar um ID único para cada div de aluno
 //      const criarAlunos = (aluno) => {
 //         const containerAlunos = document.createElement('div');
 //         containerAlunos.classList.add('containerAlunos');
-        
+
 //         const containerAluno = document.createElement('div');
 //         containerAluno.classList.add('containerAluno');
 
@@ -74,32 +74,32 @@
 //         if(aluno.status  === 'Finalizado') {containerAluno.style.backgroundColor = '#3347B0'}
 //         else{  containerAluno.style.backgroundColor = ' #E5B657'}
 
-       
 
-         
-    
+
+
+
 //         const img = document.createElement('img')
 //         img.classList.add('imgAluno')
 //         img.src = `./img/aluno.png`
-    
+
 //         const name = document.createElement('span')
 //         name.classList.add('nameAluno')
 //         name.textContent = aluno.nome
-    
+
 //         containerAluno.append(img, name);
 //         containerAlunos.append(containerAluno);
-        
+
 //  idIncremental++; // incrementa a variável incremental para gerar um novo ID único para a próxima div de aluno
 
 //         return containerAlunos;
 //     }
-    
+
 //     const carregarAlunos = () => {
 //         const container = document.getElementById('alunos');
 //         const cntatoI = alunos.map(criarAlunos);
 //         container.append(...cntatoI);
 //     }
-    
+
 //     carregarAlunos();
 
 //     // -----------------------------------------------
@@ -108,7 +108,7 @@
 //     // const aluno = () => {
 //     //     const containerAlunos = document.createElement('div');
 //     //     containerAlunos.classList.add('alunoselecionado');
-       
+
 
 //     //     const img = document.createElement('img')
 //     //     img.classList.add('imgalunoescolhido')
@@ -119,7 +119,7 @@
 //     //     name.textContent = aluno.nome
 
 //     //     containerAluno.append(img, name);
-        
+
 //     // }
 
 //     // const carregaAlunoIndividual = () => {
@@ -130,7 +130,7 @@
 
 //     // carregaAlunoIndividual()
 'use strict'
-import {alunos} from "./alunos.js"
+// import { alunos } from "./alunos.js"
 
 
 // const criarAlunos = (aluno) => {
@@ -139,7 +139,7 @@ import {alunos} from "./alunos.js"
 
 //     const containerAluno = document.createElement('div');
 //     containerAluno.classList.add('containerAluno');
-   
+
 //       const img = document.createElement('img')
 //      img.classList.add('imgAluno')
 //      img.src = `./img/aluno.png`
@@ -158,7 +158,7 @@ import {alunos} from "./alunos.js"
 //     const container = document.getElementById('alunos')
 //     const cntatoI = alunos.map(criarAlunos)
 
-    
+
 //     container.replaceChildren(...cntatoI)
 // }
 
@@ -169,11 +169,13 @@ const urlCurso = "http://localhost:8080/v1/lion-school/cursos";
 async function getCursos() {
   const response = await fetch(urlCurso);
   const data = await response.json();
+  console.log(data)
   const cursos = data.cursos.map(curso => {
     console.log(data)
     return {
       nome: curso.nome_curso,
-      icone: curso.icone_curso
+      icone: curso.icone_curso,
+      sigla: curso.sigla_curso
     };
   });
   return cursos;
@@ -198,92 +200,176 @@ const criarMateria = (materia) => {
 
 const carregarAula = async () => {
 
-  
+
   const container = document.getElementById('classRoom');
   const cursos = await getCursos();
   const cntatoI = cursos.map(criarMateria);
   container.append(...cntatoI);
+
+  const room = document.querySelectorAll('.room');
+  const main = document.getElementById('couses');
+  const main2 = document.getElementById('studants');
+
+  main.style.overflow = 'auto'
+  main.style.height = '100%'
+
+  main2.style.overflow = 'auto'
+  main2.style.height = '100%'
+
+
+  // const main3 = document.getElementById('terciario')
+  const main3 = document.getElementById('student')
+  const span = document.getElementById("t");
+  const headerSecondary = document.getElementById('headerSecondary')
+
+
+  // main3.style.overflow = 'auto'
+  // main3.style.height ='100%'
+  room.forEach(function (element) {
+    console.log(element)
+    element.addEventListener('click', function () {
+
+      main.style.display = 'none';
+      headerSecondary.style.display = "flex"
+      main2.style.display = 'block';
+
+      const span = document.getElementById("t");
+      if (span.textContent === "Sair") {
+        span.textContent = "voltar"
+      } else {
+        span.textContent = "Sair"
+      }
+      // Obter o texto da classe a ser usado como novo texto
+      const newClassName = this.querySelector(".nameClassRoom").textContent;
+
+      // Atualizar o texto da classe nameClassRoom com o novo texto
+      const nameClassRoom = document.querySelector(".nameClassRoomescolhida");
+      nameClassRoom.textContent = newClassName;
+
+    });
+  });
+
+  span.addEventListener('click', function () {
+    if (span.textContent === "Sair") {
+
+
+    } else {
+      if (span.textContent === "voltar" && main2.style.display == 'block') {
+
+        main.style.display = 'flex';
+        headerSecondary.style.display = "none"
+        main3.style.display = 'none'
+        main2.style.display = 'none';
+        span.textContent = "Sair"
+
+      }
+
+      else {
+        main2.style.display = 'block';
+        main3.style.display = 'none'
+        main.style.display = 'none';
+        headerSecondary.style.display = "flex"
+      }
+
+    }
+  });
 }
-
-
 
 carregarAula();
+// ---------------------------------------------------------------------------------------
+const urlgetlistStudents = "http://localhost:8080/v1/lion-school/alunos"
+async function getAlunos() {
+  const response = await fetch(urlgetlistStudents)
+  // console.log(response)
+  const data = await response.json()
+  console.log("-------------------------------------------")
+  console.log(data)
+  console.log("-------------------------------------------")
+  const alunos = data.alunos.map(aluno =>{
+    return {
+      nome_aluno: aluno.nome_aluno,
+      image_aluno: aluno.image_aluno  
+    }
+  })
+ return alunos
+}
+getAlunos()
 
-  
 
 let idIncremental = 1; // variável para gerar um ID único para cada div de aluno
- const criarAlunos = (aluno) => {
-    const containerAlunos = document.createElement('div');
-    containerAlunos.classList.add('containerAlunos');
-    
-    const containerAluno = document.createElement('div');
-    containerAluno.classList.add('containerAluno');
+const criarAlunos = (aluno) => {
+  const containerAlunos = document.createElement('div');
+  containerAlunos.classList.add('containerAlunos');
 
-    // containerAluno.id = 'containerAluno'
+  const containerAluno = document.createElement('div');
+  containerAluno.classList.add('containerAluno');
 
-    containerAluno.id = `containerAluno-${idIncremental}`; // utiliza a variável incremental para gerar um ID único
+  // containerAluno.id = 'containerAluno'
 
-    if(aluno.status  === 'Finalizado') {containerAluno.style.backgroundColor = '#3347B0'}
-    else{  containerAluno.style.backgroundColor = ' #E5B657'}
+  containerAluno.id = `containerAluno-${idIncremental}`; // utiliza a variável incremental para gerar um ID único
 
-   
+  // if (aluno.status === 'Finalizado') { containerAluno.style.backgroundColor = '#3347B0' }
+  // else { containerAluno.style.backgroundColor = ' #E5B657' }
 
-     
 
-    const img = document.createElement('img')
-    img.classList.add('imgAluno')
-    img.src = `./img/aluno.png`
 
-    const name = document.createElement('span')
-    name.classList.add('nameAluno')
-    name.textContent = aluno.nome
 
-    containerAluno.append(img, name);
-    containerAlunos.append(containerAluno);
 
-    
-    
-idIncremental++; // incrementa a variável incremental para gerar um novo ID único para a próxima div de aluno
+  const img = document.createElement('img')
+  img.classList.add('imgAluno')
+  img.src = `./img/aluno.png`
 
-    return containerAlunos;
+  const name = document.createElement('span')
+  name.classList.add('nameAluno')
+  name.textContent = aluno.nome_aluno
+
+  containerAluno.append(img, name);
+  containerAlunos.append(containerAluno);
+
+
+
+  idIncremental++; // incrementa a variável incremental para gerar um novo ID único para a próxima div de aluno
+
+  return containerAlunos;
 }
 
-const carregarAlunos = () => {
-    const container = document.getElementById('alunos');
-    const cntatoI = alunos.map(criarAlunos);
-    container.append(...cntatoI);
+const carregarAlunos = async () => {
+  const container = document.getElementById('alunos');
+  const alunos = await getAlunos();
+  const cntatoI = alunos.map(criarAlunos);
+  container.append(...cntatoI);
 }
 
 carregarAlunos();
 const opcoesStatus = document.querySelector('#div-opcoes');
 
 opcoesStatus.addEventListener('click', (event) => {
-const opcaoSelecionada = event.target.textContent.trim();
+  const opcaoSelecionada = event.target.textContent.trim();
 
-if (opcaoSelecionada === 'Finalizado') {
-const alunosFinalizados = alunos.filter(aluno => aluno.status === 'Finalizado');
-const container = document.getElementById('alunos');
-container.innerHTML = '';
-const alunosFiltrados = alunosFinalizados.map(criarAlunos);
+  if (opcaoSelecionada === 'Finalizado') {
+    const alunosFinalizados = alunos.filter(aluno => aluno.status === 'Finalizado');
+    const container = document.getElementById('alunos');
+    container.innerHTML = '';
+    const alunosFiltrados = alunosFinalizados.map(criarAlunos);
 
-container.append(...alunosFiltrados);
-}
-else if (opcaoSelecionada === 'Cursando') {
-const alunosCursando = alunos.filter(aluno => aluno.status === 'Cursando');
-const container = document.getElementById('alunos');
-container.innerHTML = '';
-const alunosFiltrados = alunosCursando.map(criarAlunos);
+    container.append(...alunosFiltrados);
+  }
+  else if (opcaoSelecionada === 'Cursando') {
+    const alunosCursando = alunos.filter(aluno => aluno.status === 'Cursando');
+    const container = document.getElementById('alunos');
+    container.innerHTML = '';
+    const alunosFiltrados = alunosCursando.map(criarAlunos);
 
-container.append(...alunosFiltrados);
+    container.append(...alunosFiltrados);
 
-} else if (opcaoSelecionada === 'Status') {
-const alunosCursando = alunos
-const container = document.getElementById('alunos');
-container.innerHTML = '';
-const alunosFiltrados = alunosCursando.map(criarAlunos);
+  } else if (opcaoSelecionada === 'Status') {
+    const alunosCursando = alunos
+    const container = document.getElementById('alunos');
+    container.innerHTML = '';
+    const alunosFiltrados = alunosCursando.map(criarAlunos);
 
-container.append(...alunosFiltrados);
-}
+    container.append(...alunosFiltrados);
+  }
 });
 
 
@@ -293,7 +379,7 @@ container.append(...alunosFiltrados);
 // const aluno = () => {
 //     const containerAlunos = document.createElement('div');
 //     containerAlunos.classList.add('alunoselecionado');
-   
+
 
 //     const img = document.createElement('img')
 //     img.classList.add('imgalunoescolhido')
@@ -304,7 +390,7 @@ container.append(...alunosFiltrados);
 //     name.textContent = aluno.nome
 
 //     containerAluno.append(img, name);
-    
+
 // }
 
 // const carregaAlunoIndividual = () => {
@@ -325,22 +411,12 @@ container.append(...alunosFiltrados);
 //     console.log(data)
 //     console.log("-------------------------------------------")
 // return data.cursos
-  
+
 //   }
 
 //   getCursos()
 
-  const urlgetlistStudents= "http://localhost:8080/v1/lion-school/alunos"
-  async function getAlunos() {
-    const response = await fetch(urlgetlistStudents)
-    // console.log(response)
-    const data = await response.json()
-    console.log("-------------------------------------------")
-    console.log(data)
-    console.log("-------------------------------------------")
- 
-  }
-  getAlunos()
 
-  
- 
+
+
+
--- telas.js
+++ telas.js
@@ -137,107 +137,107 @@
 'use strict'
 
 
-				var divPrincipal = document.getElementById("status");
-				var divOpcoes = document.getElementById("div-opcoes");
+// 				var divPrincipal = document.getElementById("status");
+// 				var divOpcoes = document.getElementById("div-opcoes");
 
-				divPrincipal.addEventListener("click", function () {
-					if (divOpcoes.style.display === "none") {
-						divOpcoes.style.display = "block";
+// 				divPrincipal.addEventListener("click", function () {
+// 					if (divOpcoes.style.display === "none") {
+// 						divOpcoes.style.display = "block";
 						
-					} else {
-						divOpcoes.style.display = "none";
-					}
-				});
+// 					} else {
+// 						divOpcoes.style.display = "none";
+// 					}
+// 				});
 
-				// divOpcoes.addEventListener("click", function () {
-				// 	if (divOpcoes.textContent = "Status") {
-				// 		main2.backgroundColor = 'red'
-				// 	} else {
+// 				// divOpcoes.addEventListener("click", function () {
+// 				// 	if (divOpcoes.textContent = "Status") {
+// 				// 		main2.backgroundColor = 'red'
+// 				// 	} else {
 						
-				// 	}
-				// });
+// 				// 	}
+// 				// });
 		
 		
 
-// ---------------------------------------------------------------------------
-// troca da 1 tela para a segunda tels
+// // ---------------------------------------------------------------------------
+// // troca da 1 tela para a segunda tels
 
-			const room = document.querySelectorAll('.room');
-			const main = document.getElementById('couses');
-			const main2 = document.getElementById('studants');
+// 			const room = document.querySelectorAll('.room');
+// 			const main = document.getElementById('couses');
+// 			const main2 = document.getElementById('studants');
 
-			main.style.overflow = 'auto'
-			main.style.height ='100%'
+// 			main.style.overflow = 'auto'
+// 			main.style.height ='100%'
 
-			main2.style.overflow = 'auto'
-			main2.style.height ='100%'
+// 			main2.style.overflow = 'auto'
+// 			main2.style.height ='100%'
 
 		
-		// const main3 = document.getElementById('terciario')
-		const main3 = document.getElementById('student')
-			const span = document.getElementById("t");
-			const headerSecondary = document.getElementById('headerSecondary')
+// 		// const main3 = document.getElementById('terciario')
+// 		const main3 = document.getElementById('student')
+// 			const span = document.getElementById("t");
+// 			const headerSecondary = document.getElementById('headerSecondary')
 
 			
-			// main3.style.overflow = 'auto'
-			// main3.style.height ='100%'
-			room.forEach(function (element) {
-				console.log(element)
-				element.addEventListener('click', function () {
+// 			// main3.style.overflow = 'auto'
+// 			// main3.style.height ='100%'
+// 			room.forEach(function (element) {
+// 				console.log(element)
+// 				element.addEventListener('click', function () {
 
-					main.style.display = 'none';
-					headerSecondary.style.display = "flex"
-					main2.style.display = 'block';
+// 					main.style.display = 'none';
+// 					headerSecondary.style.display = "flex"
+// 					main2.style.display = 'block';
 
-					const span =document.getElementById("t");
-					if(span.textContent ===  "Sair") {
-						span.textContent = "voltar"
-					}else {
-						span.textContent = "Sair"
-					}
-// Obter o texto da classe a ser usado como novo texto
-const newClassName = this.querySelector(".nameClassRoom").textContent;
+// 					const span =document.getElementById("t");
+// 					if(span.textContent ===  "Sair") {
+// 						span.textContent = "voltar"
+// 					}else {
+// 						span.textContent = "Sair"
+// 					}
+// // Obter o texto da classe a ser usado como novo texto
+// const newClassName = this.querySelector(".nameClassRoom").textContent;
 
-// Atualizar o texto da classe nameClassRoom com o novo texto
-const nameClassRoom = document.querySelector(".nameClassRoomescolhida");
-nameClassRoom.textContent =newClassName;
+// // Atualizar o texto da classe nameClassRoom com o novo texto
+// const nameClassRoom = document.querySelector(".nameClassRoomescolhida");
+// nameClassRoom.textContent =newClassName;
 					
-				});
-			});
+// 				});
+// 			});
 
 			
-			span.addEventListener('click', function () {
-				if(span.textContent ==="Sair") {
+// 			span.addEventListener('click', function () {
+// 				if(span.textContent ==="Sair") {
 
 
-				}else{
-					if(span.textContent ==="voltar" && main2.style.display == 'block') {
+// 				}else{
+// 					if(span.textContent ==="voltar" && main2.style.display == 'block') {
 					
-						main.style.display = 'flex';
-						headerSecondary.style.display = "none"
-						main3.style.display = 'none'
-						main2.style.display = 'none';
-						span.textContent = "Sair"
+// 						main.style.display = 'flex';
+// 						headerSecondary.style.display = "none"
+// 						main3.style.display = 'none'
+// 						main2.style.display = 'none';
+// 						span.textContent = "Sair"
 		
 		
 						
 		
-						}
+// 						}
 						
-						else {
-							main2.style.display = 'block';
-							main3.style.display = 'none'
-							main.style.display = 'none';
-							headerSecondary.style.display = "flex"
-						}
+// 						else {
+// 							main2.style.display = 'block';
+// 							main3.style.display = 'none'
+// 							main.style.display = 'none';
+// 							headerSecondary.style.display = "flex"
+// 						}
 
-				}
+// 				}
 
 			
 
 
 				
-			  });
+// 			  });
 		
 //----------------------------------------------------
 
